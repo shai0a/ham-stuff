3b4a77859402277723600b9050b07d7465faf4db
shai0a <105570315+shai0a@users.noreply.github.com>
2022-05-24 10:07:54 -0500

    add description and usage

--- callsign/getLicenses.sh
+++ callsign/getLicenses.sh
@@ -2,9 +2,16 @@
 
 # Requires xml2
 
+# Outputs CSV of liceness matching callsign
+
 # https://www.fcc.gov/reports-research/developers/license-view-api
 
 URL="http://data.fcc.gov/api/license-view/basicSearch/getLicenses?searchValue="
 FIELDS="licName frn callsign categoryDesc serviceDesc statusDesc expiredDate licenseID licDetailURL"
+
+if [ $# -ne 1 ]; then
+	echo "Usage: $0 <callsign>" >&2
+	exit 1
+fi
 
 curl --silent --show-error --location $URL$1 | xml2 | 2csv Licenses/License $FIELDS
