a39c1d4efaee4f8f080eb9b856f38ad49b745b6d
shai0a <105570315+shai0a@users.noreply.github.com>
2022-05-24 10:04:12 -0500

    record duplicate argument for record from var for fields var

--- callsign/getLicenses.sh
+++ callsign/getLicenses.sh
@@ -5,6 +5,6 @@
 # https://www.fcc.gov/reports-research/developers/license-view-api
 
 URL="http://data.fcc.gov/api/license-view/basicSearch/getLicenses?searchValue="
-FIELDS="Licenses/License licName frn callsign categoryDesc serviceDesc statusDesc expiredDate licenseID licDetailURL"
+FIELDS="licName frn callsign categoryDesc serviceDesc statusDesc expiredDate licenseID licDetailURL"
 
 curl --silent --show-error --location $URL$1 | xml2 | 2csv Licenses/License $FIELDS
