144fc1bfc70a1ad3e3a1abc54bb3ef7cccca1315
shai0a <105570315+shai0a@users.noreply.github.com>
2022-11-10 15:52:50 -0600

    fix argument flags

--- amsat/amsat.org/web/track.sh
+++ amsat/amsat.org/web/track.sh
@@ -2,12 +2,12 @@
 
 # Available satellites at www.amsat.org/track/api/v1/passes.php?objects or view the options for the satellite selection in the HTML
 
-if [ $# -ne 2 ]; then
+if [ $# -lt 1 ]; then
 	echo "usage: $0 <grid square> <satellite>" >&2
 	exit 1
 fi
 
 # Alternatively use lat= & lng= instead of loc, where lat is latitude N and lng is longitude W
-curl -F loc=${1} satellite=${2:-ISS} -F count=50 -F -F doPredict=%20Predict%20 https://www.amsat.org/track/index.php \
+curl -F loc=${1} -F satellite=${2:-ISS} -F count=50 -F doPredict=%20Predict%20 https://www.amsat.org/track/index.php \
 	| grep -A1 '<th.*Date' | sed 's/<br>/ /g' | html2text -b 0 \
 	| sed 's/|/,/g'
