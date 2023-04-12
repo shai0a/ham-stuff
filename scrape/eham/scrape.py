47a20b9e01c30cae2ed79ead638e6f8fb2e943ff
shai0a <105570315+shai0a@users.noreply.github.com>
2023-04-12 06:24:39 -0500

    eham scrape script

---
+++ scrape/eham/scrape.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.eham.net/classifieds'
+
+r = requests.get(url)
+
+s = BeautifulSoup(r.content, "html.parser")
+
+for div in s.find_all('div', attrs={'class': 'nicebox'}):
+    h3 = div.find('h3')
+    if h3 is not None:
+        a = h3.find('a')
+        if a is not None:
+            print('/'.join([url, a.get('href')]), a.get_text())
