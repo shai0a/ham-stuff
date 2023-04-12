69875ab6f4a4fa6870ef16f176c94d54b12d2376
shai0a <105570315+shai0a@users.noreply.github.com>
2023-04-12 09:09:05 -0500

    require both gridsquare + satellite name and capitalize them

--- amsat/amsat.org/web/track.sh
+++ amsat/amsat.org/web/track.sh
@@ -2,12 +2,15 @@
 
 # Available satellites at www.amsat.org/track/api/v1/passes.php?objects or view the options for the satellite selection in the HTML
 
-if [ $# -lt 1 ]; then
+if [ $# -lt 2 ]; then
 	echo "usage: $0 <grid square> <satellite>" >&2
 	exit 1
 fi
 
+loc=$(printf '%s\n' "$1" | awk '{print toupper($1)}')
+sat=$(printf '%s\n' "$2" | awk '{print toupper($1)}')
+
 # Alternatively use lat= & lng= instead of loc, where lat is latitude N and lng is longitude W
-curl -F loc=${1} -F satellite=${2:-ISS} -F count=50 -F doPredict=%20Predict%20 https://www.amsat.org/track/index.php \
+curl -s -F loc=$loc -F satellite=$sat -F count=50 -F doPredict=%20Predict%20 https://www.amsat.org/track/index.php \
 	| grep -A1 '<th.*Date' | sed 's/<br>/ /g' | html2text -b 0 \
 	| sed 's/|/,/g'
